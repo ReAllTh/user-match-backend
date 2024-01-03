f0aa8c011799a881a598630b79eb2dd5d59e7566
ReAllTh <z1197487156@gmail.com>
2024-01-03 18:01:02 +0800

    set up knife4j to auto generate backend api doc

---
+++ src/main/java/link/reallth/usermatchbackend/config/Knife4jConfig.java
@@ -0,0 +1,23 @@
+package link.reallth.usermatchbackend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Info info = new Info();
+        info.setTitle("用户匹配系统");
+        info.setDescription("用户匹配系统后端接口文档");
+        info.setVersion("0.0.1");
+        info.setTermsOfService("TODO");
+        License mit = new License().name("MIT").url("https://choosealicense.com/licenses/mit/");
+        info.setLicense(mit);
+
+        return new OpenAPI().info(info);
+    }
+}
